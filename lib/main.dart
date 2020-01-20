e71371c20e05418f51fa0d923b51189a920081b2
Youngermaster <jmyoungh@eafit.edu.co>
2020-01-20 16:50:54 -0500

    Version 1 finished

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter_clock_helper/customizer.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   if (!kIsWeb && Platform.isMacOS) {
     debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   }
 
-  runApp(ClockCustomizer((ClockModel model) => DigitalClock(model)));
+  runApp(ClockCustomizer((ClockModel model) => FlutterClock(model)));
 }
 
 class FlutterClock extends StatefulWidget {
@@ -23,6 +24,7 @@ class FlutterClock extends StatefulWidget {
 class _FlutterClockState extends State<FlutterClock> {
   DateTime _dateTime = DateTime.now();
   Timer _timer;
+  bool isSeparator = true;
 
   @override
   void initState() {
@@ -55,12 +57,10 @@ class _FlutterClockState extends State<FlutterClock> {
 
   void _updateTime() {
     setState(() {
+      isSeparator = !isSeparator;
       _dateTime = DateTime.now();
-
       _timer = Timer(
-        Duration(minutes: 1) -
-            Duration(seconds: _dateTime.second) -
-            Duration(milliseconds: _dateTime.millisecond),
+        Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
         _updateTime,
       );
     });
@@ -68,6 +68,76 @@ class _FlutterClockState extends State<FlutterClock> {
 
   @override
   Widget build(BuildContext context) {
-    return Text(widget.model.weatherString);
+    final hour =
+        DateFormat(widget.model.is24HourFormat ? 'HH' : 'hh').format(_dateTime);
+    final minute = DateFormat('mm').format(_dateTime);
+    final seconds = num.parse(DateFormat('ss').format(_dateTime));
+    Widget h1 = Text(
+      hour[0],
+      style: Theme.of(context).textTheme.display4,
+    );
+    Widget h2 = Text(
+      hour[1],
+      style: Theme.of(context).textTheme.display4,
+    );
+    Widget m1 = Text(
+      minute[0],
+      style: Theme.of(context).textTheme.display4,
+    );
+    Widget m2 = Text(
+      minute[1],
+      style: Theme.of(context).textTheme.display4,
+    );
+
+    Widget separator = Text(
+      ":",
+      style: Theme.of(context).textTheme.display4,
+    );
+
+    Widget noSeparator = Text(
+      " ",
+      style: Theme.of(context).textTheme.display4,
+    );
+
+    double barWidth = MediaQuery.of(context).size.width * seconds / 60.0;
+    return Column(children: <Widget>[
+      Container(
+        height: 4.0,
+        width: barWidth,
+        color: Colors.blueAccent,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          h1,
+          h2,
+          isSeparator ? separator : noSeparator,
+          m1,
+          m2
+        ],
+      ),
+      Container(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: <Widget>[
+              Text(widget.model.temperatureString),
+              Text(" - "),
+              Text(widget.model.weatherString)
+            ],
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: <Widget>[
+            Icon(Icons.location_on),
+            VerticalDivider(width: 5.0,),
+            Text(widget.model.location)
+          ],
+        ),
+      )
+    ]);
   }
 }
